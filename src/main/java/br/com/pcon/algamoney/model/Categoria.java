a79660094f77cd526b6233525ab214f9c034d7f2
Ana Paula Lima <ana.slima6@gmail.com>
2020-06-08 23:20:35 -0300

    Aula 3.10

--- src/main/java/br/com/pcon/algamoney/model/Categoria.java
+++ src/main/java/br/com/pcon/algamoney/model/Categoria.java
@@ -5,6 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 
 @Entity
 @Table(name = "categoria")
@@ -13,6 +17,10 @@ public class Categoria {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
+
+	@NotBlank
+	@NotNull
+	@Size(min = 3, max = 20)
 	private String nome;
 
 	public Long getCodigo() {
--- src/main/java/br/com/pcon/algamoney/resource/CategoriaResource.java
+++ src/main/java/br/com/pcon/algamoney/resource/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -57,7 +58,7 @@ public class CategoriaResource {
 	 */
 
 	@PostMapping
-	public ResponseEntity<Categoria> criar(@RequestBody Categoria entity, HttpServletResponse response) {
+	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria entity, HttpServletResponse response) {
 		Categoria categoria = categoriaRepository.save(entity);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
 				.buildAndExpand(categoria.getCodigo()).toUri();
